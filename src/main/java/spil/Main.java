ff52ca2d57273e2ccf7e8e365fa3769b5775d996
Mads Møller Jensen <mads1606@gmail.com>
2018-10-11 19:47:41 +0200

    Fixes in files after merge

--- src/main/java/spil/Main.java
+++ src/main/java/spil/Main.java
@@ -1,8 +1,12 @@
 package spil;
 import gui_main.GUI;
+import main.java.spil.Game;
 
 public class Main {
     public static void main(String[] args) {
+
         GUI gui = new GUI();
+        Game.main();
     }
+
 }
