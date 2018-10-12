75921b3e66e06af5ac032cbf0e4c48b78b2c410e
Mads Møller Jensen <mads1606@gmail.com>
2018-10-12 08:50:50 +0200

    Found the commit where the game works. Commiting it fr ease of access.

--- src/main/java/spil/Player.java
+++ src/main/java/spil/Player.java
@@ -1,19 +1,50 @@
 
 package spil;
+
+
+import java.awt.*;
+
 public class Player {
 
+    private static Roll roll = new Roll();
     private static int numberOfPlayers = 0;
-    String name;
+    final String NAME;
+
+    private int score;
     private boolean won;
+    private boolean rolledDoubble;
+    private Roll lastRoll = roll;
 
     //The method being invoked by the Game when it's the players turns.
     public void main() {
-        System.out.println(name);
+        do{
+            rolledDoubble = false;
+            roll.RollDice();
+
+            if (roll.IsDoubleValue())
+                rolledDoubble = true;
+
+            score += roll.GetAddedScore();
+
+            if (roll.isSnakesEyes())
+                score = 0;
+
+            System.out.println(NAME + " score: " + score);
+
+            if (CheckWin()) {
+                won = true;
+                return;
+            }
+
+            lastRoll = roll;
+
+        } while (rolledDoubble);
     }
 
     public Player(){
         won = false;
-        name = "Player" + ++numberOfPlayers;
+        score = 0;
+        NAME = "Player " + ++numberOfPlayers;
     }
 
     public void SetWon(boolean arg){
@@ -24,5 +55,12 @@ public class Player {
         return won;
     }
 
-    //Brug SecureRandom rand = new SecureRandom();
+    public boolean CheckWin(){
+        if (score >= 40)
+            return true;
+        if (lastRoll.GetAddedScore() == 12 && roll.GetAddedScore() == 12) //checks if both dice in both rolls were 6'
+            return true;
+        else
+            return false;
+    }
 }
